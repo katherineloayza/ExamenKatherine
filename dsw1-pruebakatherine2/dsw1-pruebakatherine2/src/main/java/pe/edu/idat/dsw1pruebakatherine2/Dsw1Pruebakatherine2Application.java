24fb61c9870a022110926a9e02d31c6a53116a51
katherineloayza <katherineloayza8@gmail.com>
2023-06-08 21:33:17 -0500

    Primera Inicializacion

---
+++ dsw1-pruebakatherine2/dsw1-pruebakatherine2/src/main/java/pe/edu/idat/dsw1pruebakatherine2/Dsw1Pruebakatherine2Application.java
@@ -0,0 +1,13 @@
+package pe.edu.idat.dsw1pruebakatherine2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Dsw1Pruebakatherine2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Dsw1Pruebakatherine2Application.class, args);
+	}
+
+}
---
+++ dsw1-pruebakatherine2/dsw1-pruebakatherine2/src/test/java/pe/edu/idat/dsw1pruebakatherine2/Dsw1Pruebakatherine2ApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.idat.dsw1pruebakatherine2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Dsw1Pruebakatherine2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
